52858af18e1bb417b94193618fe7fcaa31cd526b
npsedhain <npsedhain@gmail.com>
2020-03-28 21:32:05 +0545

    Added start feature as well.

--- controllers/update.controller.js
+++ controllers/update.controller.js
@@ -3,7 +3,7 @@ const service = require("../updateService");
 const getUpdates = async (req, res) => {
   const updates = await service.getData();
 
-  const { district, covidTest } = req.query;
+  const { district, covidTest, start } = req.query;
 
   let hospitals = updates;
 
@@ -17,7 +17,17 @@ const getUpdates = async (req, res) => {
     hospitals = hospitals.filter(hospital => hospital.covidTest);
   }
 
-  res.status(200).json({ length: hospitals.length, hospitals });
+  let reduced = hospitals;
+  let length = hospitals.length;
+
+  if (start && !covidTest) {
+    const from = (start - 1) * 10;
+    const to = from + 10;
+    reduced = hospitals.slice(from, to);
+    length = length - start * 10;
+  }
+
+  res.status(200).json({ length, hospitals: reduced });
 };
 
 module.exports = {
